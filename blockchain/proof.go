d4cdc76a87cfa38ca32dc3fffb9e2db45d264f95
Valdir Torres <valdyrtorres@yahoo.com.br>
2023-01-01 21:09:50 -0300

    começo da implementação do proof of work

---
+++ blockchain/proof.go
@@ -0,0 +1,11 @@
+package blockchain
+
+// algorithm
+// Take the Data from the Block
+// Create a counter (nonce) which starts at 0
+// Create a Hash of the Data plus the Counter
+// Check the Hash to see if it meets a set of requirements
+// Requirements:
+// The First few bytes must contain 0s
+
+const Difficulty = 12
--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"github.com/tensor-programming/golang-blockchain/blockchain"
 )
 
 func main() {
-	fmt.Println(quote.Hello())
+
+	chain := blockchain.InitBlockchain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 }
